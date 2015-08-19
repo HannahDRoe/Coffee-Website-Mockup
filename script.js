3d99fbbaeeff34ee053ebb10367b0c31bcb68fe6
Hannah Roe <hannahdroe@gmail.com>
2015-08-19 12:29:57 -0600

    Nav bar now changes when scrolling.

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-$("#aboutUs").mouseenter(function(){
-    alert("You entered p1!");
+$(document).ready(function(){       
+   var scroll_start =0;
+   var startchange = $('#aboutUs');
+   var offset = startchange.offset();
+    if (startchange.length){
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $(".navbar").css('background-color', 'rgba(239, 246, 253, 0.99)');
+       } else {
+           $(".navbar").css('background-color', 'transparent');
+       }
+   });
+    }
 });
