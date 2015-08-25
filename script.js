ff3e3f8080de7155a55711ecdc25b5ff6bab6acb
Hannah Roe <hannahdroe@gmail.com>
2015-08-25 14:42:26 -0600

    Edited the back to the top button. Added script to trigger js when in large screen. Adjusted about us columns and added space above our people.

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
+if($(window).width() >= 768){
+    $(document).ready(function(){       
+       var scroll_start = 0;
+       var startchange = $('#startChange');
+       var offset = startchange.offset();
+        if (startchange.length){
+       $(document).scroll(function() { 
+          scroll_start = $(this).scrollTop();
+          if(scroll_start > offset.top) {
+              $(".navbar-fixed-top").css('background-color', 'rgba(99, 124, 150, 0.99)');
+           } else {
+               $(".navbar-fixed-top").css('background-color', 'transparent');
+           }
+       });
+        }
+    });
 
-$(document).ready(function(){       
-   var scroll_start = 0;
-   var startchange = $('#startChange');
-   var offset = startchange.offset();
-    if (startchange.length){
-   $(document).scroll(function() { 
-      scroll_start = $(this).scrollTop();
-      if(scroll_start > offset.top) {
-          $(".navbar-fixed-top").css('background-color', 'rgba(99, 124, 150, 0.99)');
-       } else {
-           $(".navbar-fixed-top").css('background-color', 'transparent');
-       }
-   });
-    }
-});
+    // Back to top button 
+    var amountScrolled = 2500;
 
-// Back to top button 
-var amountScrolled = 2500;
+    $(window).scroll(function(){
+        if ($(window).scrollTop() > amountScrolled) {
+            $("#backToTop").fadeIn('slow');
+        } else {
+            $("#backToTop").fadeOut('slow');
+        }
+    });
 
-$(window).scroll(function(){
-    if ($(window).scrollTop() > amountScrolled) {
-        $("#backToTop").fadeIn('slow');
-    } else {
-        $("#backToTop").fadeOut('slow');
-    }
-});
-
-$(document).ready(function(){
-  $('ul li a').scroll(function(){
-    $('li a').removeClass("active");
-    $(this).addClass("active");
-  });
-});
+    $(document).ready(function(){
+      $('ul li a').scroll(function(){
+        $('li a').removeClass("active");
+        $(this).addClass("active");
+      });
+    });
+}
