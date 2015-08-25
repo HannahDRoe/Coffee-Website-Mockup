a5da7cf65a0543e1e0a4669c77b2f0d80c95f154
Hannah Roe <hannahdroe@gmail.com>
2015-08-24 19:18:44 -0600

    Carousel indicators no longer showing through navbar

--- script.js
+++ script.js
@@ -27,11 +27,8 @@ $(window).scroll(function(){
 });
 
 $(document).ready(function(){
-  $('ul li a').click(function(){
+  $('ul li a').scroll(function(){
     $('li a').removeClass("active");
     $(this).addClass("active");
   });
 });
-
-
-$("body").scrollspy()
